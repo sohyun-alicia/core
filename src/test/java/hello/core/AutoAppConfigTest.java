d95843406857192381c8b841438dc78af9aeaec1
sohyun <wsh@peopulley.com>
2022-03-15 18:48:09 +0900

    - 빈등록 초기화, 소멸 메소드 - @PostConstruct, @PreDestroy 어노테이션 - Custom init, close 메서드 활용

---
+++ src/test/java/hello/core/AutoAppConfigTest.java
@@ -0,0 +1,23 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AutoAppConfigTest {
+    
+    @Test
+    void basicScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+        
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    
+        OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
+    }
+}
